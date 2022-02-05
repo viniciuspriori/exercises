da8e0a420ecadff2d3764ba897aa951f7f6160d1
viniciuspriori <82621517+viniciuspriori@users.noreply.github.com>
2022-02-05 15:17:12 -0300

    perimeter squares

---
+++ code wars/CW_PerimeterSquares/CW_PerimeterSquares/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_PerimeterSquares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger integer = new BigInteger(7);
+
+            var result = SumFct.perimeter(integer);
+
+            //var fibo = SumFct.fibo(5);
+        }
+
+
+    }
+}
