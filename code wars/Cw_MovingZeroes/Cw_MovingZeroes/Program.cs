7b241e872978e3474b4d17b4f910d938ab8f1d28
viniciuspriori <82621517+viniciuspriori@users.noreply.github.com>
2022-02-12 16:31:11 -0300

    bugadao kk pqp

---
+++ code wars/Cw_MovingZeroes/Cw_MovingZeroes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw_MovingZeroes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = Kata.MoveZeroes(new int[] { 0, 2, 0, -1, 0, 1, 0, 3, 0, 1, -4, 0, 0, 7, 7, 8, 0, 44, 67, 0 });
+        }
+    }
+}
